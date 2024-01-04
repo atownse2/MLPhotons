124970b89c2a47f3d2e442021069b710210afcd2
Austin Townsend <atownse2@nd.edu>
2024-01-04 15:21:07 -0500

    Delete ml_photons directory

--- ml_photons/python/run.sh
+++
@@ -1,4 +0,0 @@
-rm test/out*
-#cmsRun Prod_ml_photons.py placeholder PAT test/
-cmsRun Prod_ml_photons.py test/test_qcd.root PAT test/
-cmsRun Prod_flattener.py test/RUCLU_tree_test_qcd.root PAT 1 1
